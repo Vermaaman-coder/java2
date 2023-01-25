5265bc651ae0e86625e8118c036be4b3600ad3dc
Aman verma <119961159+Vermaaman-coder@users.noreply.github.com>
2023-01-25 14:56:55 +0530

    Add files via upload

---
+++ Con.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.awt.BorderLayout;
+
+public class Con {
+
+    public static void setLayout(BorderLayout borderLayout) {
+    }
+
+}
---
+++ app.java
@@ -0,0 +1,8 @@
+package app;
+
+public class app{
+    public static void main(String args[])
+    {
+        NotePad notep=new NotePad();
+    }
+}
